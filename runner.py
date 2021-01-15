ac310d92b0519e5118cae27396043d63c1b7f64f
BrianBell422 <brianbell.uk@gmail.com>
2021-01-15 12:21:48 +0000

    Initial commit

---
+++ runner.py
@@ -0,0 +1,12 @@
+# import modules.calculator as calc
+# from calculator import *
+
+from modules.calculator import *
+
+def add(number):
+    my_list = [1, 2, 3]
+    my_list.append(number)
+
+print(add(10, 20))
+print(subtract(20, 10))
+print(multiply(2, 3))
---
+++ start_code/modules/input.py
@@ -0,0 +1,3 @@
+def user_input1():
+    option = input("Select an option 1, 2, 3, 4, 5 or (Q)uit: ")
+    return option
